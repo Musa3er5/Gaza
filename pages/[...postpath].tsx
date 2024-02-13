b2d411bd8e53b6281fa7a8c2f98bc22ae5dc6a40
Musa3er5 <159795433+Musa3er5@users.noreply.github.com>
2024-02-13 09:25:18 -0800

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,109 +4,109 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-	const graphQLClient = new GraphQLClient(endpoint);
-	const referringURL = ctx.req.headers?.referer || null;
-	const pathArr = ctx.query.postpath as Array<string>;
-	const path = pathArr.join('/');
-	console.log(path);
-	const fbclid = ctx.query.fbclid;
+    const endpoint = 'https://news74hj.000webhostapp.com/graphql'
+    const graphQLClient = new GraphQLClient(endpoint);
+    const referringURL = ctx.req.headers?.referer || null;
+    const pathArr = ctx.query.postpath as Array<string>;
+    const path = pathArr.join('/');
+    console.log(path);
+    const instagramclid = ctx.query.instagramclid;
 
-	// redirect if facebook is the referer or request contains fbclid
-		if (referringURL?.includes('facebook.com') || fbclid) {
+    // redirect if Instagram is the referer or request contains instagramclid
+    if (referringURL?.includes('instagram.com') || instagramclid) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: `${
+                    `https://www.profitablegatecpm.com/gi43z8qw5w?key=324b19a46c57405976b0ded9551a6a25` + encodeURI(path as string)
+                }`,
+            },
+        };
+    }
 
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
-				}`,
-			},
-		};
-		}
-	const query = gql`
-		{
-			post(id: "/${path}/", idType: URI) {
-				id
-				excerpt
-				title
-				link
-				dateGmt
-				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
-				featuredImage {
-					node {
-						sourceUrl
-						altText
-					}
-				}
-			}
-		}
-	`;
+    const query = gql`
+        {
+            post(id: "/${path}/", idType: URI) {
+                id
+                excerpt
+                title
+                link
+                dateGmt
+                modifiedGmt
+                content
+                author {
+                    node {
+                        name
+                    }
+                }
+                featuredImage {
+                    node {
+                        sourceUrl
+                        altText
+                    }
+                }
+            }
+        }
+    `;
 
-	const data = await graphQLClient.request(query);
-	if (!data.post) {
-		return {
-			notFound: true,
-		};
-	}
-	return {
-		props: {
-			path,
-			post: data.post,
-			host: ctx.req.headers.host,
-		},
-	};
+    const data = await graphQLClient.request(query);
+    if (!data.post) {
+        return {
+            notFound: true,
+        };
+    }
+    return {
+        props: {
+            path,
+            post: data.post,
+            host: ctx.req.headers.host,
+        },
+    };
 };
 
 interface PostProps {
-	post: any;
-	host: string;
-	path: string;
+    post: any;
+    host: string;
+    path: string;
 }
 
 const Post: React.FC<PostProps> = (props) => {
-	const { post, host, path } = props;
+    const { post, host, path } = props;
 
-	// to remove tags from excerpt
-	const removeTags = (str: string) => {
-		if (str === null || str === '') return '';
-		else str = str.toString();
-		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-	};
+    // to remove tags from excerpt
+    const removeTags = (str: string) => {
+        if (str === null || str === '') return '';
+        else str = str.toString();
+        return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
+    };
 
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content={post.title} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
-				<title>{post.title}</title>
-			</Head>
-			<div className="post-container">
-				<h1>{post.title}</h1>
-				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
-				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
-			</div>
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <meta property="og:title" content={post.title} />
+                <meta property="og:description" content={removeTags(post.excerpt)} />
+                <meta property="og:type" content="article" />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:site_name" content={host.split('.')[0]} />
+                <meta property="article:published_time" content={post.dateGmt} />
+                <meta property="article:modified_time" content={post.modifiedGmt} />
+                <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+                <meta
+                    property="og:image:alt"
+                    content={post.featuredImage.node.altText || post.title}
+                />
+                <title>{post.title}</title>
+            </Head>
+            <div className="post-container">
+                <h1>{post.title}</h1>
+                <img
+                    src={post.featuredImage.node.sourceUrl}
+                    alt={post.featuredImage.node.altText || post.title}
+                />
+                <article dangerouslySetInnerHTML={{ __html: post.content }} />
+            </div>
+        </>
+    );
 };
 
 export default Post;
